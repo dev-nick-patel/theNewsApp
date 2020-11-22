0cf9830dc39631b1267f9f95dfb053562df97ab2
dev-nick-patel <nikunjpatel.au@gmail.com>
2020-11-22 16:50:59 +1100

    Internet Connection Check - Wifi - Mobile - Ethernet

---
+++ app/src/main/java/com/techand/thenewsapp/NewsApplication.kt
@@ -0,0 +1,5 @@
+package com.techand.thenewsapp
+
+import android.app.Application
+
+class NewsApplication : Application(){}
--- app/src/main/java/com/techand/thenewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/techand/thenewsapp/ui/NewsActivity.kt
@@ -2,6 +2,7 @@ package com.techand.thenewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -16,10 +17,11 @@ class NewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         setContentView(R.layout.activity_news)
 
         val newsRepository = NewsRepository(ArticleDatabase(this))
-        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
         viewModel = ViewModelProvider(this,viewModelProviderFactory).get(NewsViewModel::class.java)
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
