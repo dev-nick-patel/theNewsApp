07bc564e4ac57655f9268f45c4f9ba89ff56f9eb
dev-nick-patel <nikunjpatel.au@gmail.com>
2020-11-28 15:01:22 +1100

    - Drawable change when news article is saved.

--- app/src/main/java/com/techand/thenewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/techand/thenewsapp/ui/fragments/ArticleFragment.kt
@@ -3,6 +3,8 @@ package com.techand.thenewsapp.ui.fragments
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebViewClient
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.snackbar.Snackbar
@@ -10,11 +12,13 @@ import com.techand.thenewsapp.R
 import com.techand.thenewsapp.ui.NewsActivity
 import com.techand.thenewsapp.ui.NewsViewModel
 import kotlinx.android.synthetic.main.fragment_article.*
+import java.nio.channels.Selector
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
     private lateinit var viewModel: NewsViewModel
     private val args: ArticleFragmentArgs by navArgs()
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -26,6 +30,10 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         }
 
         fab.setOnClickListener{
+            DrawableCompat.setTint(
+                    DrawableCompat.wrap(fab.getDrawable()),
+                    ContextCompat.getColor(activity as NewsActivity, android.R.color.holo_red_dark)
+            );
             viewModel.saveArticle(article)
             Snackbar.make(view, "Article saved successfully", Snackbar.LENGTH_SHORT).show()
         }
--- app/src/main/java/com/techand/thenewsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/techand/thenewsapp/ui/fragments/SearchNewsFragment.kt
@@ -31,6 +31,7 @@ class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         viewModel = (activity as NewsActivity).viewModel
         setUpRecycleView()
 
